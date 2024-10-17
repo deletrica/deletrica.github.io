d6106c29fb77af1b1ae2329e4914ffe539b82a1b
deletrica <delvani.eletrica@gmail.com>
2024-10-17 03:54:56 -0300

    update

--- static/js/module.js
+++ static/js/module.js
@@ -39,7 +39,7 @@ function openSocial(label) { location.assign(Profile.social[label]); }
 $(window).on('load', function(evt) {
   qrcode.instance = qrcode(vcard({ fn: Profile.name, tel: Profile.tel }));
 
-  if (share.support) $('[data-btn="share"]').removeAttr('hidden');
+  if (share.support) $('[data-btn="share"]').parent().removeAttr('hidden');
   $('[data-btn]').click(function(evt) {
     evt.preventDefault();
     if (this.dataset.btn === 'tel') openDial();
