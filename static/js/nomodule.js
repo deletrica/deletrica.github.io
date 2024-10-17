5c4953dd6c791c7a5fb7c62b3194a139737140a0
deletrica <delvani.eletrica@gmail.com>
2024-10-17 17:40:48 -0300

    updates…

--- static/js/nomodule.js
+++ static/js/nomodule.js
@@ -1,11 +1,8 @@
-$(window).on('load', function(evt) {
-  /*$.dialog({
-    type: 'red',
-    title: 'Your browser is out of date!',
-    content: '<div class="text-secondary"><div>Your browser is not supported!</div><div>Update your browser to view this website correctly.</div></div>',
-    closeIcon: false
-  });*/
+/*
+ => Your browser is not supported!
+ => Update your browser to view this website correctly.
+*/
 
-  if ('replace' in this.location) location.replace('/update.html');
-  else this.location.href = '/update.html';
-})
+window.location.replace(
+  '/update.html'
+);
