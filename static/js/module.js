f3a7736ecd5079ea19aa41d7eb1b9038a867a897
deletrica <delvani.eletrica@gmail.com>
2024-10-18 05:00:39 -0300

    update

--- static/js/module.js
+++ static/js/module.js
@@ -5,6 +5,8 @@ import vcard from './modules/vcard.js';
 import qrcode from './modules/qrcode.js';
 import Profile from './modules/profile.js';
 
+
+let style, blobURL, domContentLoaded;
 const dialog = {
   openDial() {
     location.assign(`tel:${Profile.tel}`);
@@ -39,29 +41,33 @@ const dialog = {
 };
 
 $(window).on('load', function(evt) {
+  style = document.createElement('style');
   qrcode.instance = qrcode(vcard({ fn: Profile.name, tel: Profile.tel }));
+  domContentLoaded = () => {
+    if (navigator.isMobile()) {
+      Profile.social.whatsapp = 'whatsapp://' + (new URL(Profile.social.whatsapp)).pathname.slice(1);
+      Profile.social.telegram = 'tg:resolve?domain=' + (new URL(Profile.social.telegram)).pathname.slice(1);
+    }
 
-  if (navigator.isMobile()) {
-    Profile.social.whatsapp = 'whatsapp://' + (new URL(Profile.social.whatsapp)).pathname.slice(1);
-    Profile.social.telegram = 'tg:resolve?domain=' + (new URL(Profile.social.telegram)).pathname.slice(1);
-  }
+    if (share.support) $('[data-btn="share"]').parent().removeAttr('hidden');
 
-  if (share.support) $('[data-btn="share"]').parent().removeAttr('hidden');
-  $('[data-btn]').click(function(evt) {
-    evt.preventDefault();
-    if (this.dataset.btn === 'tel') dialog.openDial();
-    else if (this.dataset.btn === 'share') dialog.openShare();
-    else if (this.dataset.btn === 'qrcode') dialog.openVCard();
-    else if (/^(telegram|whatsapp)$/.test(this.dataset.btn)) dialog.openSocial(this.dataset.btn);
-  });
+    $('[data-btn]').click(function(evt) {
+      evt.preventDefault();
+      if (this.dataset.btn === 'tel') dialog.openDial();
+      else if (this.dataset.btn === 'share') dialog.openShare();
+      else if (this.dataset.btn === 'qrcode') dialog.openVCard();
+      else if (/^(telegram|whatsapp)$/.test(this.dataset.btn)) dialog.openSocial(this.dataset.btn);
+    });
 
-  $(document.body).waitForImages(function() {
-    qrcode.instance.getRawData()
-      .then(blob => {
-        $('<img/>').on('load error', function(evt) {
-          $($('[data-template="qrcode"]')[0].content).find('[data-bg="qrcode"]').css('background-image', `url("${this.src}")`);
-          $('[data-page=splash]').fadeOut(undefined, function(evt) {});
-        }).attr('src', URL.createObjectURL(blob));
-      })
+    $('[data-page-splash]').fadeOut();
+  };
+
+  qrcode.instance.getRawData().then(blob => {
+    blobURL = URL.createObjectURL(blob);
+    style.innerHTML = `.qr-code { background-image: url("${blobURL}"); }`;
+
+    $(document.head).append(style);
+    $(document.body).prepend($('[data-template="profile"]').html())
+      .waitForImages(domContentLoaded);
   });
 });
