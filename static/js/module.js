9ac495078eb4408336af762b8ee75829eee7d181
deletrica <delvani.eletrica@gmail.com>
2024-10-18 05:44:33 -0300

    update module.js

--- static/js/module.js
+++ static/js/module.js
@@ -1,13 +1,14 @@
 "use strict";
 
+let dialog, domReady;
+
 import share from './modules/share.js';
 import vcard from './modules/vcard.js';
 import qrcode from './modules/qrcode.js';
 import Profile from './modules/profile.js';
 
-
-let style, blobURL, domContentLoaded;
-const dialog = {
+qrcode.instance = qrcode(vcard({ fn: Profile.name, tel: Profile.tel }));
+dialog = {
   openDial() {
     location.assign(`tel:${Profile.tel}`);
   },
@@ -40,34 +41,29 @@ const dialog = {
   openSocial(label) { location.assign(Profile.social[label]); }
 };
 
+domReady = () => {
+  if (navigator.isMobile()) {
+    Profile.social.whatsapp = 'whatsapp://' + (new URL(Profile.social.whatsapp)).pathname.slice(1);
+    Profile.social.telegram = 'tg:resolve?domain=' + (new URL(Profile.social.telegram)).pathname.slice(1);
+  }
+
+  if (share.support) $('[data-btn="share"]').parent().removeAttr('hidden');
+
+  $('[data-btn]').click(function(evt) {
+    evt.preventDefault();
+    if (this.dataset.btn === 'tel') dialog.openDial();
+    else if (this.dataset.btn === 'share') dialog.openShare();
+    else if (this.dataset.btn === 'qrcode') dialog.openVCard();
+    else if (/^(telegram|whatsapp)$/.test(this.dataset.btn)) dialog.openSocial(this.dataset.btn);
+  });
+
+  $('[data-page-splash]').fadeOut();
+};
+
 $(window).on('load', function(evt) {
-  style = document.createElement('style');
-  qrcode.instance = qrcode(vcard({ fn: Profile.name, tel: Profile.tel }));
-  domContentLoaded = () => {
-    if (navigator.isMobile()) {
-      Profile.social.whatsapp = 'whatsapp://' + (new URL(Profile.social.whatsapp)).pathname.slice(1);
-      Profile.social.telegram = 'tg:resolve?domain=' + (new URL(Profile.social.telegram)).pathname.slice(1);
-    }
-
-    if (share.support) $('[data-btn="share"]').parent().removeAttr('hidden');
-
-    $('[data-btn]').click(function(evt) {
-      evt.preventDefault();
-      if (this.dataset.btn === 'tel') dialog.openDial();
-      else if (this.dataset.btn === 'share') dialog.openShare();
-      else if (this.dataset.btn === 'qrcode') dialog.openVCard();
-      else if (/^(telegram|whatsapp)$/.test(this.dataset.btn)) dialog.openSocial(this.dataset.btn);
-    });
-
-    $('[data-page-splash]').fadeOut();
-  };
-
+  $(document.body).prepend($('[data-template="profile"]').html());
   qrcode.instance.getRawData().then(blob => {
-    blobURL = URL.createObjectURL(blob);
-    style.innerHTML = `.qr-code { background-image: url("${blobURL}"); }`;
-
-    $(document.head).append(style);
-    $(document.body).prepend($('[data-template="profile"]').html())
-      .waitForImages(domContentLoaded);
+    $(document.head).append($('<style></style>').append(`.qr-code { background-image: url("${URL.createObjectURL(blob)}"); }`));
+    $(document.body).waitForImages(domReady);
   });
 });
