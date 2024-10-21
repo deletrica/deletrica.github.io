05f7fe2c21e2bb9c4425dc30117cc956d2c19ecc
deletrica <delvani.eletrica@gmail.com>
2024-10-21 15:18:48 -0300

    Update qrcode.js

--- static/js/modules/qrcode.js
+++ static/js/modules/qrcode.js
@@ -4,7 +4,7 @@ const qrcode = data => {
     width: 512,
     height: 512,
     type: 'svg',
-    image: '/static/img/svg/message-phone-chat-icon.svg',
+    image: '/static/img/svg/tel-icon.svg',
     qrOptions: {
       errorCorrectionLevel: 'M'
     },
