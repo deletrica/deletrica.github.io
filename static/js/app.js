b1e78a1cf18d1ab8733b21fc691d40eaaa40c14a
deletrica <delvani.eletrica@gmail.com>
2024-10-24 18:10:09 -0300

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,8 @@
 function dialogSocial(evt) {
   evt.preventDefault();
 
-  if (this.id === 'btnTelegram') location.assign('https://t.me/deletric');
+  if (this.id === 'btnSignal') location.assign('https://signal.me/#eu/JxNPlMl9PayTPSGVFeLg8bsCkhsuTH9Whom2DK7_Jm7lNsiufgdAx7xo6aY0C8W1');
+  else if (this.id === 'btnTelegram') location.assign('https://t.me/deletric');
   else if (this.id === 'btnWhatsApp') location.assign('https://wa.me/message/NJFMCEDAZEMTN1');
 }
 
